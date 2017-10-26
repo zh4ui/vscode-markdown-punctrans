887d41e91f9453a3999f601a6323291971e83e46
Hui <zh4ui@users.noreply.github.com>
2017-10-26 22:50:54 +0800

    temporary check in

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-const CONFIG_NAME = 'tabconvertleftchar';
+const CONFIG_NAME = 'tabReplaceChar';
 
 let g_config = vscode.workspace.getConfiguration(CONFIG_NAME);
+
+// TODO: should put into a smaller scope
+let g_langTable : object;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,7 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
     );
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('tabConvertLeftChar', tabConvertLeftChar)
+        vscode.commands.registerCommand('zh4ui.tabreplacechar', tabReplaceChar)
     );
 }
 
@@ -29,25 +32,41 @@ export function deactivate() {
 }
 
 function reloadConfiguration() {
-    console.log("reload")
     g_config = vscode.workspace.getConfiguration(CONFIG_NAME);
 }
 
+function isConfigured(editor: vscode.TextEditor): boolean {
+    if (!g_config.get<boolean>('enabled')) {
+        return false;
+    }
+    let langId = editor.document.languageId;
+    let langTable = g_config.get<object>('langConversionTable')[langId];
+    if (langTable == null) {
+        return false;
+    }
+    return true;
+}
 function getReplaceChar(inputChar: string): string {
-    let table = g_config.get<object>('markdown');
-    let replaceChar = table[inputChar]
+    let replaceChar = g_langTable[inputChar]
     if (replaceChar == null) {
         replaceChar = inputChar;
     }
     return replaceChar;
 }
-function tabConvertLeftChar() {
+function tabReplaceChar() {
+
     let activeTextEditor = vscode.window.activeTextEditor
     if (!activeTextEditor) return;
 
-    const { document, selections } = activeTextEditor
-
     let delegateToTabCommand = true;
+
+    if (!isConfigured(activeTextEditor)) {
+        // XXX bad smell of code
+        vscode.commands.executeCommand("tab");
+        return;
+    }
+
+    const { document, selections } = activeTextEditor
 
     activeTextEditor.edit((editBuilder) => {
         for (let selection of selections) {
